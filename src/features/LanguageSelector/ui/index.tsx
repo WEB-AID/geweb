e629b0a9b9e21afb8e8b9e48baac964c668d6edb
WEB-AID <web.aid.dev@gmail.com>
2024-11-27 12:20:03 +0100

    IOS phases bug fix3 commit

--- src/features/LanguageSelector/ui/index.tsx
+++ src/features/LanguageSelector/ui/index.tsx
@@ -18,7 +18,7 @@ export function LanguageSelector({ className }: LanguageSelectorProps) {
     const pathname = usePathname() || ''
     const currentLocale = i18n.language
     const isBurgerOpen = useBurgerMenuStore((store) => store.isBurgerOpen)
-    const closeBurger = useBurgerMenuStore((store) => store.closeBurger)
+    const openBurger = useBurgerMenuStore((store) => store.openBurger)
 
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const newLocale = event.target.value
@@ -29,7 +29,7 @@ export function LanguageSelector({ className }: LanguageSelectorProps) {
         document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/;SameSite=Lax`
 
         if (isBurgerOpen) {
-            closeBurger()
+            openBurger()
         }
 
         if (currentLocale === i18nConfig.defaultLocale) {
