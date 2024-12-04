8c9e55ee234be910ead66ed48dcdabfe3ddd279d
WEB-AID <web.aid.dev@gmail.com>
2024-12-04 21:08:21 +0100

    test4 commit

--- src/pages/home-page/ui/MapBlock/index.tsx
+++ src/pages/home-page/ui/MapBlock/index.tsx
@@ -13,7 +13,7 @@ const Map = () => {
     }
     return (
         <div className="w-3/4 lg:w-3/5 mx-auto flex flex-col md:flex-row gap-4 items-center justify-center my-10 lg:my-20 font-extrabold">
-            <div className="md:w-1/3 flex flex-col h-32 md:h-60">
+            <div className="md:w-1/3 flex flex-col h-32 min-[600px]:h-24 md:h-60">
                 <span className="mb-auto text-sm lg:text-base">
                     {t('mapText')}
                 </span>
