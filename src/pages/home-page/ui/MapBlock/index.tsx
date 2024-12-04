fdf891062dfc17f735955abfc18f90cfbe2641fa
WEB-AID <web.aid.dev@gmail.com>
2024-12-04 13:32:27 +0100

    swiperAndGoogleMap2 commit

--- src/pages/home-page/ui/MapBlock/index.tsx
+++ src/pages/home-page/ui/MapBlock/index.tsx
@@ -13,7 +13,7 @@ const Map = () => {
     }
     return (
         <div className="w-3/4 lg:w-3/5 mx-auto flex flex-col md:flex-row gap-4 items-center justify-center my-10 md:my-20">
-            <div className="md:w-1/3 flex flex-col h-24 md:h-60">
+            <div className="md:w-1/3 flex flex-col h-28 md:h-60">
                 <span className="mb-auto text-sm md:text-base">
                     {t('mapText')}
                 </span>
@@ -26,7 +26,7 @@ const Map = () => {
                     {t('mapButton')}
                 </Button>
             </div>
-            <div className="md:ml-12 w-full md:w-96 lg:w-1/2 flex items-center justify-center">
+            <div className="max-[767px]:mt-4 md:ml-12 w-full md:w-96 lg:w-1/2 flex items-center justify-center">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13376.617626814921!2d44.43961131019164!3d41.463423518722664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4043eb7a014c0f17%3A0x71c96f892c224569!2sAkaurta%20pig%20farm!5e1!3m2!1sru!2sua!4v1733056020151!5m2!1sru!2sua"
                     width="0"
